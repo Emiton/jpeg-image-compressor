73124c815d48a6843c562a86b5a9ed05a412aa4d
Cameron LaFreniere <cam.laf567@gmail.com>
2018-11-04 23:30:18 -0500

    Now calls functions from rgbComponentConversions

--- compress.c
+++ compress.c
@@ -1,12 +1,20 @@
 #include "compress.h"
 #include "pnm.h"
+#include "rgbComponentConversions.h"
+#include <a2plain.h>
+#include <a2methods.h>
 
 extern void compress(FILE *input)
 {
-    (void) input;     
+    (void) input;
+    A2Methods_T methods = array2_methods_plain;
+    Pnm_ppm img = Pnm_ppmread(input, methods);
+    scaledIntToFloat(img);    
 }
 
 extern void decompress(FILE *input)
 {
     (void) input;     
 }
+
+
