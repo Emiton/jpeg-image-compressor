5f42a5d5fb2e80fdb88087650d02f3e1a29af5bc
Cameron LaFreniere <cam.laf567@gmail.com>
2018-11-04 22:48:08 -0500

    Started structure of both files

---
+++ compress.c
@@ -0,0 +1,12 @@
+#include "compress.h"
+#include "pnm.h"
+
+extern void compress(FILE *input)
+{
+    (void) input;     
+}
+
+extern void decompress(FILE *input)
+{
+    (void) input;     
+}
---
+++ rgbComponentConversions.c
@@ -0,0 +1,13 @@
+#include <a2methods.h>
+#include <a2plain.h>
+#include <pnm.h>
+#include <stdio.h>
+
+typedef A2Methods_Array2 A2;
+
+//TODO: make return type A2.
+void scaledIntToFloat(Pnm_ppm img)
+{
+    (void) img;
+    printf("vegan soup\n"); 
+}   
