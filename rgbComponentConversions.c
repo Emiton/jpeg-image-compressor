716aef3a30db78a1e898abccaa4770b9a758e086
Em <emiton_alves@my.uri.edu>
2018-11-05 14:19:47 -0500

    Got denominator from image

--- rgbComponentConversions.c
+++ rgbComponentConversions.c
@@ -1,8 +1,14 @@
 #include "rgbComponentConversions.h"
 
+typedef struct rgb_float {
+    float r, g, b;
+} *rgb_float;
+
 //TODO: make return type A2.
 void scaledIntToFloat(Pnm_ppm img)
 {
+    float denom = (float) img->denominator;
+    printf("DENOM: %f\n", denom);
     (void) img;
     printf("vegan soup\n"); 
 }   
