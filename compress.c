52b237c999396e02cfdd24908e0d9b78889731b3
Cameron LaFreniere <cam.laf567@gmail.com>
2018-11-07 22:32:22 -0500

    Fixed includes

--- compress.c
+++ compress.c
@@ -3,6 +3,7 @@
 #include "rgbComponentConversions.h"
 #include <a2plain.h>
 #include <a2methods.h>
+#include "dctQuantConversions.h"
 
 #define A2 A2Methods_Array2
 
@@ -16,7 +17,8 @@ extern void compress(FILE *input)
      
     A2 rgbFloatArray = scaledIntToFloat(img);
     A2 ybrFloatArray = rgbFloatToYbrFloat(rgbFloatArray, h, w);
-    A2 someFunc(ybrFloatArray);
+    A2 quantizedArray = reduce(ybrFloatArray, h, w);
+    (void) quantizedArray;
 }
 
 extern void decompress(FILE *input)
