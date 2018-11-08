4194d84b1f397f2953d88e8c867d536021d6b2c7
Cameron LaFreniere <cam.laf567@gmail.com>
2018-11-07 22:34:26 -0500

    Created header for Direct Cosine Conversions and Quantization

---
+++ dctQuantConversions.h
@@ -0,0 +1,25 @@
+#include <assert.h>
+#include <a2plain.h>
+#include <a2methods.h>
+#include <array2.h>
+#include "arith.h"
+#include "rgbComponentConversions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define A2 A2Methods_Array2
+
+typedef struct quantizedValues {
+    signed a,b,c,d;
+    unsigned avgPb, avgPr;
+}  *quantizedValues;
+  
+typedef struct reducedValues {
+    float a,b,c,d;
+    float avgPb, avgPr;
+}  reducedValues;
+
+extern A2 reduce(A2 componentArray, int height, int width);
+float discreteCosineTransform(char coefficient, float y1, float y2, float y3, float y4);
+unsigned quantizeColorDifference(float chroma);
+#undef A2
